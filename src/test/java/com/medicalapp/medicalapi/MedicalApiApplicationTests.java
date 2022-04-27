9b68413e0f2961d9b45b7f2319b98b3560060105
21nithya <nithiyakalyani21@gmail.com>
2022-04-27 10:11:31 +0530

    medical app

---
+++ src/test/java/com/medicalapp/medicalapi/MedicalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalapp.medicalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
